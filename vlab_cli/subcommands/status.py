2da37d368528c5c33b76da5dbb2fdc7040a73448
willnx <willnx84@gmail.com>
2018-12-27 13:50:15 -0800

    updated 'vlab status' to include OneFS IPs and context for 'vlab connect'

--- vlab_cli/subcommands/status.py
+++ vlab_cli/subcommands/status.py
@@ -6,14 +6,13 @@ import click
 from tabulate import tabulate
 
 from vlab_cli.lib.api import consume_task
-from vlab_cli.lib.widgets import typewriter
+from vlab_cli.lib.widgets import typewriter, Spinner
 
 
 @click.command()
 @click.pass_context
 def status(ctx):
     """Display general information about your virtual lab"""
-
     resp = consume_task(ctx.obj.vlab_api,
                         endpoint='/api/1/inf/inventory',
                         message='Collecting information about your inventory',
@@ -25,19 +24,27 @@ def status(ctx):
             # if the gateway is off, it wont have an IP
             gateway_ip = [x for x in gateway['ips'] if ':' not in x and not x.startswith('192.168')][0]
         except IndexError:
-            gateway_ip = None
+            gateway_ip = gateway['state']
     else:
-        gateway_ip = None
-    vm_body = []
-    vm_header = ['Name', 'IPs', 'Type', 'Version', 'Powered', 'Console']
-    for vm, data in vm_info.items():
-        shorter_link = ctx.obj.vlab_api.post('/api/1/link',
-                                             json={'url': data['console']}).json()['content']['url']
-        kind = data['meta']['component']
-        version = data['meta']['version']
-        power = data['state'].replace('powered', '')
-        row = [vm, '\n'.join(data['ips']), kind, version, power, shorter_link]
-        vm_body.append(row)
+        gateway_ip = 'None' # so users see the literal word
+    with Spinner('Processing inventory records'):
+        vm_body = []
+        vm_header = ['Name', 'IPs', 'Connectable', 'Type', 'Version', 'Powered', 'Console']
+        for vm in sorted(vm_info.keys()):
+            addr_info = ctx.obj.vlab_api.get_addrs(target_name=vm)
+            connectable = addr_info.get(vm, {}).get('routable', 'initializing')
+            shorter_link = ctx.obj.vlab_api.post('/api/1/link',
+                                                 json={'url': vm_info[vm]['console']}).json()['content']['url']
+            kind = vm_info[vm]['meta']['component']
+            version = vm_info[vm]['meta']['version']
+            power = vm_info[vm]['state'].replace('powered', '')
+            ips = '\n'.join(vm_info[vm]['ips'])
+            if not ips:
+                # fall back to port map rule
+                addrs = addr_info.get(vm, {}).get('addr', '')
+                ips = '\n'.join(addrs)
+            row = [vm, ips, connectable, kind, version, power, shorter_link]
+            vm_body.append(row)
 
     heading = '\nUsername: {}\nGateway : {}\n'.format(ctx.obj.username, gateway_ip)
     vm_table = tabulate(vm_body, headers=vm_header, tablefmt='presto')
