29622ed8fccb21c831a165eca732432659f49971
willnx <willnx84@gmail.com>
2018-12-31 09:35:13 -0800

    fixed upgrade_code for MSI

--- setup_win.py
+++ setup_win.py
@@ -11,8 +11,6 @@ from vlab_cli import version
 
 packages = ['pkg_resources', 'jwt', 'idna', 'vlab_cli', 'cryptography', 'cffi']
 
-# Forces uninstall of old package when updating MSI
-sys.argv += ['--upgrade-code', '4adf8ee9-526b-4b37-a9c3-c46f42de5a53']
 if not '--add-to-path' in sys.argv:
     sys.argv += ['--add-to-path', 'True']
 
@@ -34,5 +32,6 @@ setup(name="vlab-cli",
       entry_points={'console_scripts' : 'vlab=vlab_cli.vlab:cli'},
       install_requires=['click', 'pyjwt', 'requests', 'tabulate', 'cryptography', 'colorama'],
       executables = [Executable('vlab', base=None, icon='vlab_icon.ico')],
-      options = {'build_exe' : {'packages' : packages}},
+      options = {'build_exe' : {'packages' : packages},
+                 'build_msi' : {'upgrade_code': '{4adf8ee9-526b-4b37-a9c3-c46f42de5a53}'}},
       )
