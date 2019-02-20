72a0d81631c8a4773c75b480d01f374f9b095707
willnx <willnx84@gmail.com>
2019-02-19 17:19:35 -0800

    removed remaining business logic for IPAM routing

--- vlab_cli/subcommands/show/portmap.py
+++ vlab_cli/subcommands/show/portmap.py
@@ -4,6 +4,7 @@ import click
 
 from tabulate import tabulate
 
+from vlab_cli.lib.api import consume_task
 from vlab_cli.lib.widgets import Spinner
 from vlab_cli.lib.portmap_helpers import port_to_protocol
 
@@ -11,8 +12,9 @@ from vlab_cli.lib.portmap_helpers import port_to_protocol
 @click.pass_context
 def portmap(ctx):
     """Display configured port mapping/forwarding rules"""
+    table = "No portmap rules exist"
     with Spinner('Looking up port mapping rules'):
-        rules = ctx.obj.vlab_api.get_port_map()
+        rules = ctx.obj.vlab_api.get('/api/1/ipam/portmap').json()['content']
         header = ['Name', 'Type', 'Port', 'Protocol']
         rows = []
         for conn_port, details in rules.items():
@@ -22,5 +24,5 @@ def portmap(ctx):
             protocol = port_to_protocol(vm_type, vm_port)
             rows.append([name, vm_type, conn_port, protocol])
             table = tabulate(rows, headers=header, tablefmt='presto', numalign="center")
-    click.echo('\nGateway IP: {}'.format(ctx.obj.vlab_api._ipam_ip))
+    click.echo('\nGateway IP: {}'.format(ctx.obj.vlab_api.vga_ip()))
     click.echo(table)
