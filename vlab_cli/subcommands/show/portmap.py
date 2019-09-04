bbdee263c3095e4ac387c78a17e5e205f2ab7c07
willnx <willnx84@gmail.com>
2019-09-04 08:28:20 -0700

    Added -v flag to show target ip when running 'vlab show portmap'

--- vlab_cli/subcommands/show/portmap.py
+++ vlab_cli/subcommands/show/portmap.py
@@ -9,8 +9,10 @@ from vlab_cli.lib.widgets import Spinner
 from vlab_cli.lib.portmap_helpers import port_to_protocol
 
 @click.command()
+@click.option('--verbose', '-v', is_flag=True,
+              help='Display extra info about port mapping rules')
 @click.pass_context
-def portmap(ctx):
+def portmap(ctx, verbose):
     """Display configured port mapping/forwarding rules"""
     table = "No portmap rules exist"
     with Spinner('Looking up port mapping rules'):
@@ -18,13 +20,20 @@ def portmap(ctx):
         rules = data['ports']
         gateway_ip = data['gateway_ip']
         header = ['Name', 'Type', 'Port', 'Protocol']
+        if verbose:
+            header.append('Target IP')
         rows = []
         for conn_port, details in rules.items():
             name = details.get('name', 'Error')
             vm_type = details.get('component', 'Unknown')
             vm_port = details.get('target_port', 0)
             protocol = port_to_protocol(vm_type, vm_port)
-            rows.append([name, vm_type, conn_port, protocol])
+            target_ip = details.get('target_addr', 'Unknown')
+            if verbose:
+                row = [name, vm_type, conn_port, protocol, target_ip]
+            else:
+                row = [name, vm_type, conn_port, protocol]
+            rows.append(row)
             table = tabulate(rows, headers=header, tablefmt='presto', numalign="center")
     click.echo('\nGateway IP: {}'.format(gateway_ip))
     click.echo(table)
