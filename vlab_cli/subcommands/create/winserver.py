507b98f59b591e1fbaea12ab8534b10ad18d240e
willnx <willnx84@gmail.com>
2020-05-15 11:05:25 -0700

    Updated CLI to send static IP to Winserver

--- vlab_cli/subcommands/create/winserver.py
+++ vlab_cli/subcommands/create/winserver.py
@@ -15,14 +15,29 @@ from vlab_cli.lib.ascii_output import format_machine_info
               help='The version of Microsoft Server to create')
 @click.option('-n', '--name', cls=MandatoryOption,
               help='The name of the Microsoft Server in your lab')
+@click.option('-s', '--static-ip', default='',
+              help='The static IP to assign your DNS server. Omit to obtain a DHCP address')
+@click.option('-m', '--external-netmask', default='255.255.255.0', show_default=True,
+              help='The subnet mask to use on the public network')
+@click.option('-g', '--default-gateway', default='192.168.1.1', show_default=True,
+              help='The default gateway to use on the public network')
+@click.option('-d', '--dns-servers', default=['192.168.1.1'], multiple=True, show_default=True,
+              help='The DNS server(s) to configure for the host OS')
 @click.option('-e', '--external-network', default='frontend', show_default=True,
               help='The public network to connect the new Microsoft Server to')
 @click.pass_context
-def winserver(ctx, name, image, external_network):
+def winserver(ctx, name, image, external_network, external_netmask, default_gateway,
+              dns_servers, static_ip):
     """Create a new Microsoft Server instance"""
     body = {'network': external_network,
             'name': name,
-            'image': image.upper()} # upper in case they supply 2012r2
+            'image': image.upper(), # upper in case they supply 2012r2
+            'ip-config': {'static-ip': static_ip,
+                          'default-gateway': default_gateway,
+                          'netmask': external_netmask,
+                          'dns': dns_servers
+                         }
+            }
     resp = consume_task(ctx.obj.vlab_api,
                         endpoint='/api/2/inf/winserver',
                         message='Creating a new instance of Microsoft Server {}'.format(image),
