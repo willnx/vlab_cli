b5b0a7e420a2c27912dedee9382b84a724de125a
willnx <willnx84@gmail.com>
2021-07-16 09:23:26 -0700

    Fixed bugs in 'delete everything' command

--- vlab_cli/subcommands/delete/everything.py
+++ vlab_cli/subcommands/delete/everything.py
@@ -12,17 +12,17 @@ def everything(ctx):
     """Destroy everything you own."""
     click.confirm(" Are you sure you want to destroy your entire lab?", abort=True)
     click.confirm(" Really sure? You cannot undo this, just like you cannot un-bake a cake.", abort=True)
-    consume_task(vlab_api,
+    consume_task(ctx.obj.vlab_api,
                  endpoint='/api/1/inf/power',
                  body={'machine': 'all', 'power': 'off'},
                  message='Powering down your lab',
                  timeout=300,
                  pause=5)
-    consume_task(vlab_api,
+    consume_task(ctx.obj.vlab_api,
                  endpoint='/api/1/inf/inventory',
                  message='Destroying inventory',
                  method='DELETE')
-    resp = consume_task(vlab_api,
+    resp = consume_task(ctx.obj.vlab_api,
                         endpoint='/api/2/inf/vlan',
                         message='Determining what networks you own',
                         method='GET')
@@ -30,6 +30,6 @@ def everything(ctx):
     tasks = {}
     with Spinner('Deleting networks'):
         for vlan in vlans.keys():
-            resp = vlab_api.delete('/api/2/inf/vlan', json={'vlan-name': vlan})
+            resp = ctx.obj.vlab_api.delete('/api/2/inf/vlan', json={'vlan-name': vlan})
             tasks[vlan] = resp.links['status']['url']
-        block_on_tasks(vlab_api, tasks, pause=1)
+        block_on_tasks(ctx.obj.vlab_api, tasks, pause=1)
